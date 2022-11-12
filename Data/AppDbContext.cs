56be65022f95438768fefa71b5074262304217e9
Valeria Soledad Heredia <valeheredia97@gmail.com>
2022-11-12 17:22:10 -0300

    Modelo ApplicationUser y migracion de identity para el registro de usuarios

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using E_Commerce_V2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_Commerce_V2.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext: IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
         {
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_V2.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Display(Name ="Nombre Completo")]
+        public string Nombre { get; set; }
+    }
+}
