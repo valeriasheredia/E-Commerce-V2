a7e9a202953c9750d114c1d5dc909a67c0a64680
Valeria Soledad Heredia <valeheredia97@gmail.com>
2022-11-11 23:17:17 -0300

    Cantidad de productos que hay en el carrito

---
+++ Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce_V2.Data.Carrito;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_V2.Data.ViewComponents
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+            
+    }
+}
